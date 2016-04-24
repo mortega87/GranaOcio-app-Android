1e235b0136ae2969de902d96a4e48170a33b2d7b
mortega87 <mortega87@gmail.com>
2016-04-24 20:37:48 +0200

    Arreglado problemas indentacion

--- GranaOcio/app/src/main/java/com/mario/granaocio/JSONParser.java
+++ GranaOcio/app/src/main/java/com/mario/granaocio/JSONParser.java
@@ -2,27 +2,14 @@ package com.mario.granaocio;
 
 import java.io.InputStream;
 
-
-
-/**
- * @author zeeshan0026
- */
-
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONObject;
 
 import android.util.Log;
