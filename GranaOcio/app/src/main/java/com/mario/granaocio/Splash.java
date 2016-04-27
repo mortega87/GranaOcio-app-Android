99e359dc22dae81319af173c140c30b48992fb5e
mortega87 <mortega87@gmail.com>
2016-04-27 19:27:44 +0200

    Carga datos locales segunda vez

--- GranaOcio/app/src/main/java/com/mario/granaocio/Splash.java
+++ GranaOcio/app/src/main/java/com/mario/granaocio/Splash.java
@@ -2,6 +2,7 @@ package com.mario.granaocio;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
@@ -15,6 +16,7 @@ public class Splash extends Activity {
     protected boolean active = true;
     protected int splashTime = 3000;
     static Datos datos;
+    private boolean b = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,15 @@ public class Splash extends Activity {
                     int waited = 0;
                     while(active && (waited < splashTime)){
 
-                        datos.Update(getApplicationContext());
+                        SharedPreferences sh = getSharedPreferences("GranaOcio", MODE_PRIVATE);
+                        b=sh.getBoolean("PrimeraVez",false);
+                        if(!b) {
+                            
+                            datos.Update(getApplicationContext());
+                            SharedPreferences.Editor editor = sh.edit();
+                            editor.putBoolean("PrimeraVez", true);
+                            editor.apply();
+                        }
 
                         sleep(200);
                         if(active){
